e1b13e6b9cb1eb82d8b9cd6dedc77eff2719c457
Mattia <mattiariboni@gmail.com>
2025-06-24 22:54:19 +0200

    multiclass classification

--- src/trainer/train.py
+++ src/trainer/train.py
@@ -10,20 +10,21 @@ from src.utilities.io_handler import load_data
 from src.utilities.dataset_utils import drop_nan_and_inf_values, split_data
 from src.data.preprocessor import TabnetPreprocessor
 from pytorch_tabnet.tab_model import TabNetClassifier
+from sklearn.preprocessing import LabelEncoder
 
 CONFIG_PATH = './config/short_config.json'
 
 logging.basicConfig(level=logging.INFO)
-logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 def main(args):
     """Main function for model training"""
     
     try:
         ConfigManager.load_config(CONFIG_PATH)
-        logging.info(f"Configuration loaded successfully from {CONFIG_PATH}")
+        logger.info(f"Configuration loaded successfully from {CONFIG_PATH}")
     except (FileNotFoundError, ValueError) as e:
-        logging.error(f"Error during configuration loading: {e}")
+        logger.error(f"Error during configuration loading: {e}")
         return
 
     #Get all necessary configurations
@@ -34,23 +35,28 @@ def main(args):
 
     DATA_PATH = paths_config.get("dataset_path")
     OUTPUT_DIR = paths_config.get("output_dir")
-    TARGET_COL = data_cols_config.get("target_column")
-    TARGET_CATEGORY_COL = data_cols_config.get("target_category_column")
+    TARGET_BINARY_COL = data_cols_config.get("target_column")
+    TARGET_MULTICLASS_COL = data_cols_config.get("target_category_column")
     NUMERICAL_COLS = data_cols_config.get("numerical_cols")
     CATEGORICAL_COLS = data_cols_config.get("categorical_cols")
     RANDOM_STATE = training_params.get("random_state", 42)
+
+    if args.classification == 'multiclass':
+        TARGET_COL = TARGET_MULTICLASS_COL
+    else:
+        TARGET_COL = TARGET_BINARY_COL
     
     try:
         params = all_hyperparams[args.model_size]
-        logging.info(f"Hyperparameters loaded '{args.model_size}'.")
+        logger.info(f"Hyperparameters loaded '{args.model_size}'.")
     except KeyError:
-        logging.error(f"Model size '{args.model_size}' not found.")
+        logger.error(f"Model size '{args.model_size}' not found.")
         return
 
 
     df = load_data(DATA_PATH)
 
-    train_df, val_df, test_df = split_data(df, random_state=RANDOM_STATE, target_column=TARGET_COL, target_category_column=TARGET_CATEGORY_COL)
+    train_df, val_df, test_df = split_data(df, random_state=RANDOM_STATE, target_binary_column=TARGET_BINARY_COL, target_multiclass_column=TARGET_MULTICLASS_COL, target_column=TARGET_COL)
     
     train_df = drop_nan_and_inf_values(train_df)
     val_df = drop_nan_and_inf_values(val_df)
@@ -61,28 +67,43 @@ def main(args):
     y_train = train_df[TARGET_COL]
     X_train = train_df.drop(columns=[TARGET_COL]) 
 
-    #TARGET_CATERGORY_COL has already been dropped during data splitting, maybe doing it here is better.
     y_val = val_df[TARGET_COL]
     X_val = val_df.drop(columns=[TARGET_COL])
-    
-   
 
-    logging.info(f"Dimensions: Train={X_train.shape}, Validation={X_val.shape}, Test={test_df.shape}")
+    if args.classification == 'multiclass':
+        le = LabelEncoder()
+        y_train_enc = le.fit_transform(y_train)   # fit+transform sul train
+        y_val_enc   = le.transform(y_val)         # solo transform sul val
+
+        # salva l'encoder per inference futura
+        encoder_path = os.path.join(
+            OUTPUT_DIR,
+            f'label_encoder_{args.classification}_{args.model_size}.pkl'
+        )
+        joblib.dump(le, encoder_path)
+        logger.info(f"LabelEncoder saved in: {encoder_path}")
+
+    else:  # binary
+        # nulla da "fit"; basta estrarre l'array
+        y_train_enc = y_train.values
+        y_val_enc   = y_val.values
+
+    logger.info(f"Dimensions: Train={X_train.shape}, Validation={X_val.shape}, Test={test_df.shape}")
 
     # Pre-processing
     preprocessor = TabnetPreprocessor(numerical_cols=NUMERICAL_COLS, categorical_cols=CATEGORICAL_COLS)
-    logging.info("TabnetPreprocessor fitting...")
+    logger.info("TabnetPreprocessor fitting...")
     preprocessor.fit(X_train)
     
-    logging.info("Data tranformation...")
+    logger.info("Data tranformation...")
     X_train_processed = preprocessor.transform(X_train)
     X_val_processed = preprocessor.transform(X_val)
     cat_dims, cat_idxs = preprocessor.get_tabnet_params()
     
 
-    logging.info("TabNet configuration...")
+    logger.info("TabNet configuration...")
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logging.info(f"Using device: {device}")
+    logger.info(f"Using device: {device}")
 
     clf = TabNetClassifier(
         n_d=params['n_d'], n_a=params['n_a'], n_steps=params['n_steps'],
@@ -93,32 +114,31 @@ def main(args):
         mask_type='sparsemax', device_name=device, seed=RANDOM_STATE
     )
 
-    logging.info("Starting model training...")
+    logger.info(f"Starting {args.classification} model training...")
+
+
     clf.fit(
-        X_train=X_train_processed.values, y_train=y_train.values,
-        eval_set=[(X_val_processed.values, y_val.values)],
-        eval_name=['validation'], eval_metric=['auc', 'accuracy', 'balanced_accuracy'],
+        X_train=X_train_processed.values, y_train=y_train_enc,
+        eval_set=[(X_val_processed.values, y_val_enc)],
+        eval_name=['validation'], eval_metric=['accuracy', 'balanced_accuracy'],
         max_epochs=params['max_epochs'],
         patience=training_params.get("early_stopping_patience", 20),
         batch_size=params['batch_size'],
         virtual_batch_size=training_params.get("virtual_batch_size", 128)
     )
-    logging.info("Training complete.")
+    logger.info("Training complete.")
     
     #save the model
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     
-    model_path = os.path.join(OUTPUT_DIR, f'tabnet_model_{args.model_size}')
+    model_path = os.path.join(OUTPUT_DIR, f'tabnet_{args.classification}_model_{args.model_size}')
     clf.save_model(model_path)
-    logging.info(f"Model saved in: {model_path}.zip")
+    logger.info(f"Model saved in: {model_path}.zip")
 
-    preprocessor_path = os.path.join(OUTPUT_DIR, f'preprocessor_{args.model_size}.pkl')
+    preprocessor_path = os.path.join(OUTPUT_DIR, f'preprocessor_{args.classification}_{args.model_size}.pkl')
     joblib.dump(preprocessor, preprocessor_path)
-    logging.info(f"Preprocessor saved in: {preprocessor_path}")
+    logger.info(f"Preprocessor saved in: {preprocessor_path}")
 
-    preprocessor_path = os.path.join(OUTPUT_DIR, f'preprocessor_{args.model_size}.pkl')
-    joblib.dump(preprocessor, preprocessor_path)
-    logging.info(f"Preprocessor saved in: {preprocessor_path}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="TabNet training script.")
@@ -129,5 +149,13 @@ if __name__ == '__main__':
         choices=['small', 'medium', 'large'], 
         help="Model dimensions, see config.json"
     )
+    parser.add_argument(
+        '--classification', 
+        type=str, 
+        default='binary', 
+        choices=['binary', 'multiclass'], 
+        help="Classification type: binary or multiclass"
+    )
+
     args = parser.parse_args()
     main(args)
